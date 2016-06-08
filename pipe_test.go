916e65e8dd5ec2ad427c8b6ac9559a37c405f393
Kazuhiro Kubota <k2.wanko@gmail.com>
2016-06-08 22:10:59 +0900

    Add test

---
+++ pipe_test.go
@@ -0,0 +1,51 @@
+package pipe
+
+import (
+	"log"
+	"net"
+	"os"
+	"testing"
+
+	pb "github.com/k2wanko/grpc-pipe/testdata/echo"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+var _ net.Addr = &GRPCAddr{}
+var _ net.Listener = &GRPCListener{}
+
+type echoSrv struct{}
+
+func (s *echoSrv) Echo(ctx context.Context, msg *pb.Message) (*pb.Message, error) {
+	return msg, nil
+}
+
+var testL = Listen()
+
+func TestMain(m *testing.M) {
+	s := grpc.NewServer()
+	go s.Serve(testL)
+	pb.RegisterEchoServiceServer(s, &echoSrv{})
+	defer s.Stop()
+	os.Exit(m.Run())
+}
+
+func TestListener(t *testing.T) {
+	cc, err := grpc.Dial("", grpc.WithInsecure(), testL.WithDialer())
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	c := pb.NewEchoServiceClient(cc)
+	ctx := context.Background()
+	m := &pb.Message{Value: "Test"}
+	res, err := c.Echo(ctx, m)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if text, want := m.Value, res.Value; text != want {
+		t.Errorf("text=%s; want %s", text, want)
+	}
+}
