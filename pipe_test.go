41a6ca014f82c1ae2a2e436a246877dd108f0b82
Kazuhiro Kubota <k2.wanko@gmail.com>
2016-06-09 08:46:15 +0900

    Add TestConcurrency

--- pipe_test.go
+++ pipe_test.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"sync"
 	"testing"
 
 	pb "github.com/k2wanko/grpc-pipe/testdata/echo"
@@ -20,7 +21,10 @@ func (s *echoSrv) Echo(ctx context.Context, msg *pb.Message) (*pb.Message, error
 	return msg, nil
 }
 
-var testL = Listen()
+var (
+	testL  = Listen()
+	testCc pb.EchoServiceClient
+)
 
 func TestMain(m *testing.M) {
 	s := grpc.NewServer()
@@ -48,4 +52,32 @@ func TestListener(t *testing.T) {
 	if text, want := m.Value, res.Value; text != want {
 		t.Errorf("text=%s; want %s", text, want)
 	}
+}
+
+func TestConcurrency(t *testing.T) {
+	size := 10
+	var wg sync.WaitGroup
+	wg.Add(size)
+	for i := 0; i < size; i++ {
+		go func() {
+			defer wg.Done()
+			cc, err := grpc.Dial("", grpc.WithInsecure(), testL.WithDialer())
+			if err != nil {
+				t.Fatal(err)
+				return
+			}
+			c := pb.NewEchoServiceClient(cc)
+			ctx := context.Background()
+			m := &pb.Message{Value: "Test"}
+			res, err := c.Echo(ctx, m)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if text, want := m.Value, res.Value; text != want {
+				t.Errorf("text=%s; want %s", text, want)
+			}
+		}()
+	}
+	wg.Wait()
 }
